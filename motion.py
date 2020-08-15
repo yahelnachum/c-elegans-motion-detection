2f2852197d4b07f123ed11fb23699a410ce4822e
Yahel Nachum <yahelnachum@yahoo.com>
2020-08-15 13:13:50 -0400

    Using screen / video / window properties

--- motion.py
+++ motion.py
@@ -1,21 +1,36 @@
 import numpy as np
 import cv2
 import sys
+from win32api import GetSystemMetrics
 
+# get screen properties
+screenXPositionOffset = -16
+screenWidthOffset = 15
+screenWidth = GetSystemMetrics(0) + screenWidthOffset
+screenHeight = GetSystemMetrics(1)
+
+# get video and its properties
 video = sys.argv[1]
 cap = cv2.VideoCapture(video)
-stillImage = None
+capWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+capHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-width=int(1900 * 0.4)
-height=int(1400 * 0.4)
+# calculate window properties
+windows = 2
+windowWidth = int(screenWidth / windows)
+windowHeight = int(windowWidth / capWidth * capHeight)
 
+# set window properties
 cv2.namedWindow('frame',cv2.WINDOW_NORMAL)
 cv2.namedWindow('what',cv2.WINDOW_NORMAL)
-cv2.resizeWindow('frame', width, height)
-cv2.resizeWindow('what', width, height)
-cv2.moveWindow("frame", 0,0);
-cv2.moveWindow("what", width,0);
 
+cv2.resizeWindow('frame', windowWidth, windowHeight)
+cv2.resizeWindow('what', windowWidth, windowHeight)
+
+cv2.moveWindow("frame", 0 + screenXPositionOffset, 0);
+cv2.moveWindow("what", windowWidth + screenXPositionOffset, 0);
+
+stillImage = None
 while(cap.isOpened()):
     ret, frame = cap.read()
 
@@ -39,8 +54,8 @@ while(cap.isOpened()):
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
-    frame = cv2.resize(frame, (width, height)) 
-    thresh_frame = cv2.resize(thresh_frame, (width, height)) 
+    frame = cv2.resize(frame, (windowWidth, windowHeight)) 
+    thresh_frame = cv2.resize(thresh_frame, (windowWidth, windowHeight)) 
     thresh_frame = cv2.cvtColor(thresh_frame, cv2.COLOR_GRAY2RGB)
     cv2.imshow('frame',frame)
     cv2.imshow('what',thresh_frame)
